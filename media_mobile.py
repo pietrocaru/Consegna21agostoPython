edbe113fcd52cb852a45351775d6c0fbf2df6b7e
Pietro Caruso <pietrocaruso22@gmail.com>
2024-08-21 16:40:47 +0200

    Add files via upload

---
+++ media_mobile.py
@@ -0,0 +1,43 @@
+numeri = [1,2,3,4,5,6,7,8,9,10]
+n = 3
+m = 2
+
+y = range(1,2)
+for j in y:
+    print(j)
+
+print("\n")    
+
+z = range(2,3)
+for k in z:
+    k = (k+(k-1))/m
+    print(k)
+
+print ("\n")
+
+x = range(3,11)
+for i in x:
+    i = (i+(i-1)+(i-2))/n
+    print(i)
+
+print("\n")
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
